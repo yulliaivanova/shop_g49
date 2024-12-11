36cf298c9f6c4f151a5ca33830ae485eda731107
yulliaivanova <gerda88823@gmail.com>
2024-12-11 21:58:28 +0100

    homework08-create customer

---
+++ src/main/java/ait/cohort49/shop_g49/ShopG49Application.java
@@ -0,0 +1,13 @@
+package ait.cohort49.shop_g49;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopG49Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopG49Application.class, args);
+    }
+
+}
---
+++ src/test/java/ait/cohort49/shop_g49/ShopG49ApplicationTests.java
@@ -0,0 +1,13 @@
+package ait.cohort49.shop_g49;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopG49ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
